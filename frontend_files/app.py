e13689d3f8416c0a1e6ba8ce86092aa2e06a1c9b
anilch99 <cherukuri.ak@gmail.com>
2026-08-16 07:05:02 +0000

    Update SuperKart backend

--- frontend_files/app.py
+++ frontend_files/app.py
@@ -10,98 +10,205 @@ BACKEND_URL = os.getenv(
 
 st.title("SuperKart Sales Prediction")
 
+tab1, tab2 = st.tabs([
+    "Single Prediction",
+    "Bulk Prediction"
+])
 
-left, right = st.columns(2)
+# ==========================================================
+# SINGLE PREDICTION
+# ==========================================================
+with tab1:
 
-with left:
-  product_weight = st.number_input("Product Weight", value=12.0)
-  product_sugar_content = st.selectbox(
-      "Product Sugar Content",
-      ["Low Sugar", "Regular", "No Sugar", "reg"]
-  )
-  product_allocated_area = st.number_input(
-      "Product Allocated Area",
-      value=0.05
-  )
+  left, right = st.columns(2)
 
-  product_type = st.selectbox(
-      "Product Type",
-      ["Fruits and Vegetables", "Snack Foods", "Frozen Foods", "Dairy", "Household", "Baking Goods",
-       "Canned", "Health and Hygiene", "Meat", "Soft Drinks", "Breads", "Hard Drinks", "Starchy Foods",
-       "Breakfast", "Seafood", "Others"]
-  )
+  with left:
+    product_weight = st.number_input("Product Weight", value=12.0)
+    product_sugar_content = st.selectbox(
+        "Product Sugar Content",
+        ["Low Sugar", "Regular", "No Sugar", "reg"]
+    )
+    product_allocated_area = st.number_input(
+        "Product Allocated Area",
+        value=0.05
+    )
 
-  product_mrp = st.number_input(
-      "Product MRP",
-      value=150.0
-  )
+    product_type = st.selectbox(
+        "Product Type",
+        ["Fruits and Vegetables", "Snack Foods", "Frozen Foods", "Dairy", "Household", "Baking Goods",
+        "Canned", "Health and Hygiene", "Meat", "Soft Drinks", "Breads", "Hard Drinks", "Starchy Foods",
+        "Breakfast", "Seafood", "Others"]
+    )
 
-with right:
-  store_id = st.selectbox(
-      "Store ID",
-      ["OUT001", "OUT002", "OUT003", "OUT004"]
-  )
+    product_mrp = st.number_input(
+        "Product MRP",
+        value=150.0
+    )
 
-  store_type = st.selectbox(
-      "Store Type",
-      [
-          "Grocery Store",
-          "Supermarket Type1",
-          "Supermarket Type2",
-          "Departmental Store",
-          "Food Mart"
-      ]
-  )
+  with right:
+    store_id = st.selectbox(
+        "Store ID",
+        ["OUT001", "OUT002", "OUT003", "OUT004"]
+    )
 
-  store_location_city_type = st.selectbox(
-      "Store Location City Type",
-      [
-          "Tier 1",
-          "Tier 2",
-          "Tier 3"
-      ]
-  )
+    store_type = st.selectbox(
+        "Store Type",
+        [
+            "Grocery Store",
+            "Supermarket Type1",
+            "Supermarket Type2",
+            "Departmental Store",
+            "Food Mart"
+        ]
+    )
 
-  store_size = st.selectbox(
-      "Store SIZE",
-      ["Medium", "High", "Small"]
-  )
+    store_location_city_type = st.selectbox(
+        "Store Location City Type",
+        [
+            "Tier 1",
+            "Tier 2",
+            "Tier 3"
+        ]
+    )
 
-  store_age = st.number_input(
-      "Store Age",
-      value=10
-  )
+    store_size = st.selectbox(
+        "Store SIZE",
+        ["Medium", "High", "Small"]
+    )
+
+    store_age = st.number_input(
+        "Store Age",
+        value=10
+    )
 
 
-if st.button("Predict Sales"):
+  if st.button("Predict Sales"):
 
-    payload = {
-        "ProductWeight": product_weight,
-        "ProductSugarContent": product_sugar_content,
-        "ProductAllocatedArea": product_allocated_area,
-        "ProductType": product_type,
-        "ProductMRP": product_mrp,
+      payload = {
+          "ProductWeight": product_weight,
+          "ProductSugarContent": product_sugar_content,
+          "ProductAllocatedArea": product_allocated_area,
+          "ProductType": product_type,
+          "ProductMRP": product_mrp,
 
-        "StoreId": store_id,
-        "StoreType": store_type,
-        "StoreSize": store_size,
-        "StoreAge": store_age,
-        "StoreLocationCityType": store_location_city_type
-    }
+          "StoreId": store_id,
+          "StoreType": store_type,
+          "StoreSize": store_size,
+          "StoreAge": store_age,
+          "StoreLocationCityType": store_location_city_type
+      }
 
-    try:
-        response = requests.post(
-            f"{BACKEND_URL}/v1/predict",
-            json=payload,
-            timeout=30
+      try:
+          response = requests.post(
+              f"{BACKEND_URL}/v1/predict",
+              json=payload,
+              timeout=30
+          )
+
+          if response.status_code == 200:
+              result = response.json()
+              st.success("Prediction completed")
+              st.write("Estimataed Sales Price: ", result.estimataedSalesPrice)
+          else:
+              st.error(response.text)
+
+      except Exception as e:
+          st.error(f"Backend connection failed: {e}")
+# ==========================================================
+# BULK PREDICTION
+# ==========================================================
+with tab2:
+
+    st.subheader("Bulk Sales Prediction")
+
+    st.write(
+        "Upload a CSV file containing multiple products."
+    )
+
+    uploaded_file = st.file_uploader(
+        "Upload CSV",
+        type=["csv"]
+    )
+
+    if uploaded_file is not None:
+
+        # Preview uploaded data
+        df = pd.read_csv(uploaded_file)
+
+        st.write("Uploaded Data")
+
+        st.dataframe(
+            df,
+            use_container_width=True
         )
 
-        if response.status_code == 200:
-            result = response.json()
-            st.success("Prediction completed")
-            st.write(result)
-        else:
-            st.error(response.text)
+        st.write(
+            f"Total Records: {len(df)}"
+        )
 
-    except Exception as e:
-        st.error(f"Backend connection failed: {e}")
+        if st.button("Predict Bulk Sales"):
+
+            try:
+
+                # Convert dataframe back to CSV bytes
+                csv_data = df.to_csv(
+                    index=False
+                ).encode("utf-8")
+
+                files = {
+                    "file": (
+                        uploaded_file.name,
+                        csv_data,
+                        "text/csv"
+                    )
+                }
+
+                with st.spinner(
+                    "Generating predictions..."
+                ):
+
+                    response = requests.post(
+                        f"{BACKEND_URL}/v1/predictbatch",
+                        files=files,
+                        timeout=120
+                    )
+
+                if response.status_code == 200:
+
+                    predictions = response.json()
+
+                    st.success(
+                        "Bulk prediction completed"
+                    )
+
+            
+
+                    results_df = pd.DataFrame(
+                        list(predictions.items()),
+                        columns=[
+                            "Product_Id",
+                            "Predicted_Sales"
+                        ]
+                    )
+
+                    st.subheader(
+                        "Prediction Results"
+                    )
+
+                    st.dataframe(
+                        results_df,
+                        use_container_width=True
+                    )
+                else:
+                    st.error(
+                        f"Backend Error: "
+                        f"{response.status_code}"
+                    )
+
+                    st.write(response.text)
+
+            except Exception as e:
+
+                st.error(
+                    f"Bulk prediction failed: {e}"
+                )
